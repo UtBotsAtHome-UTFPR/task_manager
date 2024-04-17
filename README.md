1e46dbceaf934c6acceda1058327fac8c9c72cbf
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2024-04-17 16:58:35 -0300

    Added launch file for recognition task and cleaned up code

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # utbots_tasks: for task control and launching base systems
 
-### - Clone repository
+## - Clone repository
+
     https://github.com/UtBotsAtHome-UTFPR/utbots_tasks.git
 
+## - Updated to use State Machine
+
+    roslaunch task_manager recognition.launch
+
+## - Old tasks
+
 ### - Task manager
+
     roslaunch task_manager task_manager.launch
 
 ### - Navigation system
+
     roslaunch base_system navigation.launch
 
 ### - Voice system
+
     roslaunch base_system voice.launch
--- task_manager/src/personal_recognition.py
+++ task_manager/src/personal_recognition.py
@@ -5,53 +5,7 @@ import os
 import smach_ros
 from std_srvs.srv import Empty
 
-
-# Wait for someone to show up, possibly voice queue or find a face in the image: 1 outcome
-
-# Run new face routine: if failed for whatever reason go to previous step and tell the user it failed
-
-# Run training: If failed try again, if fail twice go back one step
-
-# Tell the user to go to the crowd and tell the user what is the voice queue to start looking for him, and to repeat in case the robot doesn't start imediatly: 1 outcome after voice queue
-
-# TURN AROUND EVERY NOW AND THEN i GET A LITTLE BIT LONELY: 1 outcome
-
-# Run recognition
-# Only correct when only one person is identified as something other than unknown, check for that: 1 outcome
-
-# Save the recognized image: Not really a state but will have to do
-
-# Display the user: 1 outcome
-
-# Display how many people were recognized (ask yolo or look for how many faces there are, yolo should be more reliable): DONE
-
-
-# define state Foo
-class wait(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['stop_waiting'])
-
-        self.loopRate = rospy.Rate(30)
-
-        self.done_waiting = False
- 
-        #self.pub_instructions.publish("I am now waiting, please input a message to begin")
-
-    def execute(self, userdata):
-        rospy.loginfo('Executing state waiting')
-        
-        while rospy.is_shutdown() == False:
-            # Controls speed
-            self.loopRate.sleep()
-
-            self.done_waiting = False if input() == "" else True
-
-            if self.done_waiting:
-                return 'stop_waiting'
-            
-
-
-# define state Bar
+# State that calls the adding new images phase
 class new_face(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['new_face_added', 'failed'])        
@@ -69,7 +23,8 @@ class new_face(smach.State):
         except rospy.ServiceException as e:
 
             return 'failed'
-        
+
+# State that calls the training phase
 class train(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['training_done', 'failed'])        
@@ -87,13 +42,12 @@ class train(smach.State):
         except rospy.ServiceException as e:
             return 'failed'
 
+# State that calls the recognition phase and publishes marked image and people
 class recognize(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['recognized', 'failed'])        
 
     def execute(self, userdata):
-
-        #rospy.loginfo('Executing state train')
 
         rospy.wait_for_service('/utbots_face_recognition/recognize')
         try:
