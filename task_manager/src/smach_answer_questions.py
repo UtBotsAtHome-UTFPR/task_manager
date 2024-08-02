5110ed1bf76f5d882d7a0524ab703f9a0a3053a9
bzenatti <bezenatti@hotmail.com>
2024-08-02 15:56:47 -0300

    working smach_answer_questions with SimpleActionState

--- task_manager/src/smach_answer_questions.py
+++ task_manager/src/smach_answer_questions.py
@@ -1,20 +1,18 @@
 import rospy
 import smach
-import smach_ros
-from std_srvs.srv import Empty
 import actionlib
-from utbots_actions.msg import InterpretNLUAction, InterpretNLUResult
+from utbots_actions.msg import InterpretNLUAction, InterpretNLUGoal
 from smach_ros import SimpleActionState
 
 number_of_questions = 6
 
-# This state will listen and recognize which of the questions is
+# This state will count the questions
 class question_counter(smach.State):
     def __init__(self):
         smach.State.__init__(self, 
-                            outcomes=['action_understood', 'all_answered','failed'],
-                            input_keys=['client_in','question_counter'],
-                            output_keys=['client_out','question_counter_out'])        
+                            outcomes=['action_understood', 'all_answered','aborted'],
+                            input_keys=['question_counter'],
+                            output_keys=['question_counter_out'])        
 
     def execute(self, userdata):
 
@@ -24,31 +22,13 @@ class question_counter(smach.State):
             return 'all_answered'
 
         try:
-            #if client is question to be answered and not a task command
             userdata.question_counter_out = userdata.question_counter + 1
             return 'action_understood'
         
         except rospy.ROSInterruptException:
-            return 'failed'
+            return 'aborted'
         
-# This state will convert TTS of the answer to the question detected
-class listen_answer(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, 
-                            ['question_answered','failed'],
-                            input_keys=['client'])        
 
-    def execute(self, userdata):
-
-        rospy.loginfo('Executing state listen_answer')
-
-        try:
-            #result = userdata.client.get_result()
-            return 'question_answered'
-        
-        except rospy.ROSInterruptException:
-            return 'failed'
-        
 
 def main():
 
@@ -57,10 +37,12 @@ def main():
     client = actionlib.SimpleActionClient('interpret_nlu', InterpretNLUAction)
     client.wait_for_server()
 
+
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['done', 'failed'])
-    sm.userdata.client = client
     sm.userdata.counter = 0
+
+    goal = InterpretNLUGoal()
 
     # Open the container
     with sm:
@@ -69,17 +51,16 @@ def main():
                                 question_counter(), 
                                 transitions={'action_understood':'LISTEN_ANSWER',
                                             'all_answered':'done',
-                                            'failed':'failed'},
-                                remapping={ 'client_in':'client',
-                                            'client_out':'client',
-                                            'question_counter':'counter',
+                                            'aborted':'failed'},
+                                remapping={ 'question_counter':'counter',
                                             'question_counter_out':'counter'})
-        #not gonna use SimpleActionState() but maybe is better
         smach.StateMachine.add ('LISTEN_ANSWER', 
-                                listen_answer(), 
-                                transitions={'question_answered':'QUESTION_COUNTER',
-                                            'failed':'failed'},
-                                remapping={'client':'client'})
+                                SimpleActionState('interpret_nlu',
+                                                  InterpretNLUAction, 
+                                                  goal),
+                                transitions={'succeeded':'done',
+                                            'aborted':'failed',
+                                            'preempted':'done'})
 
 
     outcome = sm.execute()
