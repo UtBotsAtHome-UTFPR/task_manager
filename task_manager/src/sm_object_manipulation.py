2c0c6e078d5560133bd8899535d5e41ff623ca7b
gustavo-fardo <gustavo.fardo@hotmail.com>
2024-10-22 15:44:42 -0300

    go to shelf sketch

--- task_manager/src/sm_object_manipulation.py
+++ task_manager/src/sm_object_manipulation.py
@@ -4,8 +4,11 @@ import rospy
 import smach
 import smach_ros
 import actionlib
+import rospkg
+import yaml
 from cv_bridge import CvBridge
 import cv2
+from geometry_msgs.msg import PoseStamped
 from utbots_actions.msg import YOLODetectionAction, YOLODetectionGoal, Extract3DPointAction, Extract3DPointGoal
 from smach_ros import SimpleActionState
 from reportlab.lib.pagesizes import A4
@@ -18,27 +21,59 @@ bboxes = None
 # Initialize CvBridge to convert ROS image message to OpenCV image
 bridge = CvBridge()
 
+# Initialize the rospkg instance
+rospack = rospkg.RosPack()
+
+# Get the path to the desired package (replace 'your_package' with the actual package name)
+package_path = rospack.get_path('task_manager')
+
+def retrieve_waypoint(nametag):
+    # Open and read the YAML file
+    with open(f"{package_path}/waypoints.yaml", 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Find the pose with the given nametag
+    for pose_data in data['poses']:
+        if pose_data['nametag'] == nametag:
+            # Create a PoseStamped message
+            pose_stamped = PoseStamped()
+
+            # Fill in the header
+            pose_stamped.header.seq = pose_data['header']['seq']
+            pose_stamped.header.stamp.secs = pose_data['header']['stamp']['secs']
+            pose_stamped.header.stamp.nsecs = pose_data['header']['stamp']['nsecs']
+            pose_stamped.header.frame_id = pose_data['header']['frame_id']
+
+            # Fill in the pose (position and orientation)
+            pose_stamped.pose.position.x = pose_data['pose']['position']['x']
+            pose_stamped.pose.position.y = pose_data['pose']['position']['y']
+            pose_stamped.pose.position.z = pose_data['pose']['position']['z']
+
+            pose_stamped.pose.orientation.x = pose_data['pose']['orientation']['x']
+            pose_stamped.pose.orientation.y = pose_data['pose']['orientation']['y']
+            pose_stamped.pose.orientation.z = pose_data['pose']['orientation']['z']
+            pose_stamped.pose.orientation.w = pose_data['pose']['orientation']['w']
+
+            return pose_stamped
+
 # This state will send a nav goal to the place the objects are located
-# class go_to_shelf(smach.State):
-    # def __init__(self):
-    #     smach.State.__init__(self, 
-    #                         outcomes=['reached', 'all_answered','aborted'],
-    #                         input_keys=['waypoint'])
+class go_to_shelf(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                            outcomes=['succeeded', 'aborted'],
+                            input_keys=['waypoint'])
+        self.pub = rospy.Publisher('/bridge_navigation_to_pose/goal', PoseStamped, queue_size=1)
 
-    # def execute(self, userdata):
+    def execute(self, userdata):
 
-    #     rospy.loginfo('Executing state question_counter')
+        rospy.loginfo('Executing state go_to_shelf')
 
-    #     if userdata.question_counter >= number_of_questions:
-    #         return 'all_answered'
-
-    #     try:
-    #         userdata.question_counter_out = userdata.question_counter + 1
-    #         return 'action_understood'
+        try:
+            self.pub.publish(retrieve_waypoint("shelf"))
+            return 'succeeded'
         
-    #     except rospy.ROSInterruptException:
-    #         return 'aborted'
-
+        except rospy.ROSInterruptException:
+            return 'aborted'
 
         
 # This state will save the labeled image with objects detected by YOLOv8
@@ -138,13 +173,13 @@ def main():
 
     rospy.init_node('sm_object_manipulation', anonymous=True)
 
-    client_yolo = actionlib.SimpleActionClient('YOLO_detection', YOLODetectionAction)
-    client_yolo.wait_for_server()
+    # client_yolo = actionlib.SimpleActionClient('YOLO_detection', YOLODetectionAction)
+    # client_yolo.wait_for_server()
 
-    rospy.loginfo("[TASK] YOLO Detection server up")
+    # rospy.loginfo("[TASK] YOLO Detection server up")
 
-    client_ext = actionlib.SimpleActionClient('extract_3d_point', Extract3DPointAction)
-    client_ext.wait_for_server()
+    # client_ext = actionlib.SimpleActionClient('extract_3d_point', Extract3DPointAction)
+    # client_ext.wait_for_server()
 
     rospy.loginfo("[TASK] Extract 3D Point server up")
 
@@ -160,16 +195,10 @@ def main():
     # Open the container
     with sm:
         # State that goes to the shelf
-        # smach.StateMachine.add('GO_TO_SHELF', 
-        #                         SimpleActionState('go_to_shelf',
-        #                                             , 
-        #                                             goal=nav_goal,
-        #                                             result_slots=[]),
-        #                         transitions={'succeeded': 'ANSWERS_LOG',
-        #                                         'aborted': 'ANSWERS_LOG',
-        #                                         'preempted': 'ANSWERS_LOG'},
-        #                         remapping={'NLUInput': 'nlu_input', 
-        #                                   'NLUOutput': 'nlu_output'})
+        smach.StateMachine.add('GO_TO_SHELF', 
+                                go_to_shelf(),
+                                transitions={'succeeded': 'GET_DETECTION',
+                                            'aborted': 'failed'})
 
         # State that gets the object detections from YOLO
         smach.StateMachine.add('GET_DETECTION', 
