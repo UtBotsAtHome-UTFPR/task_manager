cb1645671d0800c13c85fd74d14c36f53701340f
gustavo-fardo <gustavo.fardo@hotmail.com>
2024-11-07 15:02:21 -0300

    Incomplete follow me

--- task_manager/src/sm_navigation_and_follow_me.py
+++ task_manager/src/sm_navigation_and_follow_me.py
@@ -6,8 +6,12 @@ import yaml
 import rospkg
 import smach_ros
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PointStamped
 from utbots_navigation.nav_main.src.smGoTo import SmGoTo
 from std_msgs.msg import String
+import time
+import math
+import tf2_ros
 
 # Initialize the rospkg instance
 rospack = rospkg.RosPack()
@@ -51,7 +55,8 @@ class enter_arena(smach.State):
     #                         outcomes=['succeeded', 'aborted'],
     #                         input_keys=['waypoint'])
 
-    # def execute(self, userdata):
+    # def execute(self, user    #         self.result = rospy.wait_for_message('/bridge_navigate_to_pose/result', String)
+#data):
 
     #     rospy.loginfo('Executing state go_to_shelf')
 
@@ -107,41 +112,86 @@ class go_to_waypoint(smach.State):
             return 'aborted'
 
 
+def find_and_follow():
+    
+    current_position = rospy.wait_for_message()
+
+    tf_buffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tf_buffer)
+
+    # Wait for the transform to become available
+    try:
+        transform = tf_buffer.lookup_transform('odom', 'camera_link', rospy.Time(0), rospy.Duration(1.0))
+
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+        rospy.loginfo(f"Transform lookup failed: {e}")
+        return 'aborted'
+    
+
+    # Transform the point to the 'odom' frame
+    point_in_camera = tf_buffer.transform(current_position, 'camera_link', rospy.Duration(1.0))
+    
+    # Calculate the angle between A and B
+    theta = math.atan2(point.point.y[1] - point_in_camera.pose.orientation.y, point.point.x - point_in_camera.pose.orientation.x)
+
+    # Create the quaternion (w, x, y, z)
+    quaternion = (math.cos(theta / 2), 0, 0, math.sin(theta / 2))
+
+    goal = PoseStamped()
+    goal.header.frame_id = "camera_link"
+    goal.pose.position.x = point.point.x
+    goal.pose.position.y = point.point.y
+    goal.pose.orientation.w = quaternion[0]
+    goal.pose.orientation.z = quaternion[3]
+
+    return goal
+    
+
 # This state initiates and manages operator following
 class follow_operator(smach.State):
-    # def __init__(self, waypoint):
-    #     smach.State.__init__(self, 
-    #                         outcomes=['succeeded', 'aborted'],
-    #                         input_keys=['waypoint'])
-    #     self.result = "" 
-    #     self.pub = rospy.Publisher('/bridge_navigate_to_pose/goal', PoseStamped, queue_size=1)
-    #     self.waypoint = waypoint
+    def __init__(self):
+        smach.State.__init__(self, 
+                            outcomes=['succeeded', 'aborted'])
+        self.result = "" 
+        self.pub_goal = rospy.Publisher('/bridge_navigate_to_pose/goal', PoseStamped, queue_size=1)
+        self.pub_bt = rospy.Publisher('/bridge_navigate_to_pose/bt', String, queue_size=1)
 
-    # def execute(self, userdata):
+    def execute(self, userdata):
 
-    #     rospy.loginfo('Executing state go_to_shelf')
+        rospy.loginfo('Executing state follow_operator')
 
-    #     try:
-    #         # Sleep briefly to ensure the message is sent
-    #         rospy.sleep(2)
-    #         rospy.loginfo("Publish waypoint")
-    #         self.pub.publish(retrieve_waypoint(self.waypoint))
+        point = rospy.wait_for_message("tópico da posição da pessoa", PointStamped)
 
-    #         self.result = rospy.wait_for_message('/bridge_navigate_to_pose/result', String)
-    #         print(self.result)
-    #         if self.result.data == "Succeeded":
-    #             self.result.data = ""
-    #             return 'succeeded'
-    #         else:
-    #             self.result.data = ""
-    #             return 'aborted'
+
+        bt = String()
+        bt.data = "follow_point"
+        self.pub_bt(bt)
+
+        time.sleep(1)
         
-    #     except rospy.ROSInterruptException:
-    #         return 'aborted'
+        self.pub_goal.publish(find_and_follow())
+
+        while(True):
+            rospy.wait_for_message('bridge_navigate_to_pose/result')
+
+            if stop_flag:
+                bt.data = ""
+                self.pub_bt(bt)
+                time.sleep(1)
+                return 'succeeded'
+
+            self.pub_goal.publish(find_and_follow())
+            
+        
+global stop_flag 
+stop_flag = False
+
+def stop_sub(msg):
+    stop_flag = True if msg.data == 'stop' else False
 
 def main():
     rospy.init_node("sm_navigation_and_follow_me")
-    
+    rospy.Subscriber('/tópico_de_falar_pare', String, stop_sub)
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['done', 'failed'])
     
